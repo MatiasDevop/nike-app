8da12634323eb8d1b423df1b881d6aac645657cf
Matt <solomatias321@gmail.com>
2025-01-05 13:25:04 +0100

    wip: add services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,14 @@
+import { services } from "../constants"
+import ServiceCard from "../components/ServiceCard"
 
+//2:07
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="flex flex-wrap justify-center gap-9 max-container ">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   )
 }
 
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,5 @@
+import { shoe8 } from '../assets/images'
+import Button  from '../components/Button'
 
 const SuperQuality = () => {
   // 2:00
@@ -6,7 +8,30 @@ const SuperQuality = () => {
     className="flex justify-between items-center 
     max-lg:flex-col gap-10 w-full max-container">
       <div className="flex flex-1 flex-col">
-        <h2></h2>
+      <h2></h2>
+        <h2 className="font-palanquin text-4xl capitalize 
+        font-bold lg:max-w-lg">
+          We Provide you 
+          <span className='text-coral-red'> Super </span>
+          <span className='text-coral-red'>Quality </span> Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <div className='mt-11'>
+        <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img 
+        src={shoe8}
+        alt="Shoe8"
+        width={570}
+        height={522}
+        className='object-contain'
+        />
       </div>
     </section>
   )
